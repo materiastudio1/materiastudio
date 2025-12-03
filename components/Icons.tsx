477c9340c162652308bf4cb2047981c2648dece3
materiastudio1 <materiastudio1.0@gmail.com>
2025-12-03 00:33:02 -0500

    Add files via upload

---
+++ components/Icons.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { 
+  Camera, 
+  SwitchCamera, 
+  ImagePlus, 
+  Sparkles, 
+  X, 
+  Download, 
+  Share2, 
+  Trash2, 
+  ChevronLeft,
+  Aperture
+} from 'lucide-react';
+
+export const Icons = {
+  Camera,
+  Switch: SwitchCamera,
+  Gallery: ImagePlus,
+  Magic: Sparkles,
+  Close: X,
+  Save: Download,
+  Share: Share2,
+  Delete: Trash2,
+  Back: ChevronLeft,
+  Shutter: Aperture
+};
